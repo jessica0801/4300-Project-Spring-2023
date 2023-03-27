aeca89b09953bcf83a43466fc5b245f58454ab89
jessica0801 <jessicacdox@gmail.com>
2023-03-27 02:13:21 -0400

    connect boolean search to db

--- backend/helpers/MySQLDatabaseHandler.py
+++ backend/helpers/MySQLDatabaseHandler.py
@@ -1,5 +1,10 @@
 import os
 import sqlalchemy as db
+import json
+import os
+import numpy as np
+from flask import Flask, render_template, request
+from flask_cors import CORS
 
 class MySQLDatabaseHandler(object):
     
@@ -47,12 +52,56 @@ class MySQLDatabaseHandler(object):
         self.query_executor(sql_file_data)
         sql_file.close()
 
+os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
+
 MYSQL_USER = "root"
 MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "productsdb"
 
 mysql_engine = MySQLDatabaseHandler(MYSQL_USER, MYSQL_USER_PASSWORD,
-                                    MYSQL_PORT, MYSQL_DATABASE)
+                                      MYSQL_PORT, MYSQL_DATABASE)
 
-print(list(mysql_engine.query_selector(f"SELECT * FROM productinfo")))
+mysql_engine.load_file_into_db()
+products = list(mysql_engine.query_selector(f"SELECT * FROM productinfo"))
+app = Flask(__name__)
+CORS(app)
+
+def product_filter(product_type):
+    keys = ["id","product_url","product_type","product_price"]
+
+    product_type = f"SELECT * FROM productinfo WHERE LOWER( product_type ) LIKE '%%{product_type.lower()}%%'"
+    query1 = mysql_engine.query_selector(product_type)
+
+    return json.dumps([dict(zip(keys,i)) for i in query1])
+
+def price_filter(product_price):
+    price = float(product_price)
+    keys = ["id","product_url","product_type","product_price"]
+
+    product_price = f"SELECT * FROM productinfo WHERE CAST(product_price as decimal(4,2)) BETWEEN '%%{price*0.5}%%' AND '%%{price*1.5}%%'"
+    query2 = mysql_engine.query_selector(product_price)
+
+    return json.dumps([dict(zip(keys,i)) for i in query2])
+
+product = products[72]
+# print(product)
+# print(product_filter(product[2]))
+print(price_filter(product[3]))
+
+@app.route("/")
+def home():
+    return render_template('base.html', title="sample html")
+
+
+@app.route("/products")
+def products_search():
+    # product_name = request.args.get("product_name")
+    # product_url = request.args.get("product_url")
+    product_type = request.args.get("product_type")
+    product_price = request.args.get("product_price")
+    # clean_ingreds = request.args.get("clean_ingreds")
+    # price = request.args.get("price")
+    return boolean_search1(product_type, product_price)
+
+app.run(debug=True)
