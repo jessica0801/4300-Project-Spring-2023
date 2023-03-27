28ac3db0344ccfa042b7ad60754266c39c1f3765
jessica0801 <jessicacdox@gmail.com>
2023-03-27 16:56:19 -0400

    comment out code mysqldbhandler

--- backend/helpers/MySQLDatabaseHandler.py
+++ backend/helpers/MySQLDatabaseHandler.py
@@ -52,57 +52,57 @@ class MySQLDatabaseHandler(object):
         self.query_executor(sql_file_data)
         sql_file.close()
 
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
+# os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 
-MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = ""
-MYSQL_PORT = 3306
-MYSQL_DATABASE = "productsdb"
+# MYSQL_USER = "root"
+# MYSQL_USER_PASSWORD = ""
+# MYSQL_PORT = 3306
+# MYSQL_DATABASE = "productsdb"
 
-mysql_engine = MySQLDatabaseHandler(MYSQL_USER, MYSQL_USER_PASSWORD,
-                                      MYSQL_PORT, MYSQL_DATABASE)
+# mysql_engine = MySQLDatabaseHandler(MYSQL_USER, MYSQL_USER_PASSWORD,
+#                                       MYSQL_PORT, MYSQL_DATABASE)
 
-mysql_engine.load_file_into_db()
-products = list(mysql_engine.query_selector(f"SELECT * FROM productinfo"))
-app = Flask(__name__)
-CORS(app)
+# mysql_engine.load_file_into_db()
+# products = list(mysql_engine.query_selector(f"SELECT * FROM productinfo"))
+# app = Flask(__name__)
+# CORS(app)
 
-def product_filter(product_type):
-    keys = ["id","product_url","product_type","product_price"]
+# def product_filter(product_type):
+#     keys = ["id","product_url","product_type","product_price"]
 
-    product_type = f"SELECT * FROM productinfo WHERE LOWER( product_type ) LIKE '%%{product_type.lower()}%%'"
-    query1 = mysql_engine.query_selector(product_type)
+#     product_type = f"SELECT * FROM productinfo WHERE LOWER( product_type ) LIKE '%%{product_type.lower()}%%'"
+#     query1 = mysql_engine.query_selector(product_type)
 
-    return json.dumps([dict(zip(keys,i)) for i in query1])
+#     return json.dumps([dict(zip(keys,i)) for i in query1])
 
-def price_filter(product_price):
-    price = float(product_price)
-    keys = ["id","product_url","product_type","product_price"]
+# def price_filter(product_price):
+#     price = float(product_price)
+#     keys = ["id","product_url","product_type","product_price"]
 
-    product_price = f"SELECT * FROM productinfo WHERE CAST(product_price as decimal(5,2)) BETWEEN '%%{price*0.5}%%' AND '%%{price*1.5}%%'"
-    query2 = mysql_engine.query_selector(product_price)
+#     product_price = f"SELECT * FROM productinfo WHERE CAST(product_price as decimal(5,2)) BETWEEN '%%{price*0.5}%%' AND '%%{price*1.5}%%'"
+#     query2 = mysql_engine.query_selector(product_price)
 
-    return json.dumps([dict(zip(keys,i)) for i in query2])
+#     return json.dumps([dict(zip(keys,i)) for i in query2])
 
-product = products[2]
-# print(product)
-# print(product_filter(product[2]))
-print(product[1])
-# print(price_filter(product[4]))
+# product = products[2]
+# # print(product)
+# # print(product_filter(product[2]))
+# print(product[1])
+# # print(price_filter(product[4]))
 
-@app.route("/")
-def home():
-    return render_template('base.html', title="sample html")
+# @app.route("/")
+# def home():
+#     return render_template('base.html', title="sample html")
 
 
-@app.route("/products")
-def products_search():
-    # product_name = request.args.get("product_name")
-    # product_url = request.args.get("product_url")
-    product_type = request.args.get("product_type")
-    product_price = request.args.get("product_price")
-    # clean_ingreds = request.args.get("clean_ingreds")
-    # price = request.args.get("price")
-    return boolean_search1(product_type, product_price)
+# @app.route("/products")
+# def products_search():
+#     # product_name = request.args.get("product_name")
+#     # product_url = request.args.get("product_url")
+#     product_type = request.args.get("product_type")
+#     product_price = request.args.get("product_price")
+#     # clean_ingreds = request.args.get("clean_ingreds")
+#     # price = request.args.get("price")
+#     return boolean_search1(product_type, product_price)
 
-app.run(debug=True)
+# app.run(debug=True)
