aedd4d553dc5f74d6ec661f8ef3aced4904372d8
Dikkila Sherpa <Dikkila@Dikkilas-MacBook-Pro-2.local>
2023-03-27 13:19:38 -0400

    Cosine

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 import json
+import math
 import os
+import re
+import string
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
+
 
 # ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
@@ -50,5 +54,108 @@ def episodes_search():
     text = request.args.get("product_name")
     return sql_search(text)
 
+def tokenize(text):
+    """text is a string. tokenize(text) cleans the text and removes stopwords 
+    and punctuations.
+    returns: list of words"""
+    text = ''.join([word for word in text if word not in string.punctuation])
+    text = text.lower()
+    stopwords = ["ourselves", "hers", "between", "yourself", "but", "again", "there", 
+                 "about", "once", "during", "out", "very", "having", "with", "they", 
+                 "own", "an", "be", "some", "for", "do", "its", "yours", "such", 
+                 "into", "of", "most", "itself", "other", "off", "is", "s", "am", 
+                 "or", "who", "as", "from", "him", "each", "the", "themselves", 
+                 "until", "below", "are", "we", "these", "your", "his", "through",
+                "don", "nor", "me", "were", "her", "more", "himself", "this", "down",
+                "should", "our", "their", "while", "above", "both", "up", "to", "ours", 
+                "had", "she", "all", "no", "when", "at", "any", "before", "them", "same",
+                "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", 
+                "that", "because", "what", "over", "why", "so", "can", "did", "not", "now",
+                "under", "he", "you", "herself", "has", "just", "where", "too", "only", 
+                "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", 
+                "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", 
+                "here", "than"]
+    text = ' '.join([word for word in text.split() if word not in stopwords])
+    return re.findall("[A-Za-z]+" ,text)
+
+
+def build_inv_ind(tokenized_dict):
+    ans = {}
+    for id in range(len(tokenized_dict)):
+        doc = tokenized_dict.keys()[id]
+        tmp = {}
+        for word in tokenized_dict[doc]:
+            if word not in tmp:
+                tmp[word]=0
+            tmp[word]+=1
+        for word in tmp:
+            if word not in ans:
+                ans[word] = []
+            ans[word] += [(id, tmp[word])]
+    return ans
+
+def compute_idf(inv_ind, num_products, min_df, max_df_ratio):
+    ans = {}
+    for word in inv_ind.keys():
+        contains = len(inv_ind[word])
+        if min_df <= contains and float(contains)/num_products <= max_df_ratio:
+            ans[word]= round(math.log2(num_products / float(1+contains)),2)
+    return ans
+
+def compute_norms(index, idf, num_products):
+    ans = [0 for _ in num_products]
+    for word in index.keys():
+        if word in idf:
+            for pair in index[word]:
+                ans[pair[0]] += (pair[1] * idf[word])**2
+    
+    for i in range(num_products):
+        ans[i] = math.sqrt(ans[i])
+    return ans
+
+#query_word_counts: example_query_words = {"like": 2, "mother": 1, "daughter": 1}
+def acc_dot_scores(query_word_counts, index, idf):
+    ans={}
+    for word in query_word_counts.keys():
+        tf = query_word_counts[word]
+        if word in idf:
+            for pair in index[word]:
+                if pair[0] not in ans:
+                    ans[pair[0]] = 0
+                ans[pair[0]] += tf * pair[1] * (idf[word]**2)
+    return ans
+
+def index_search(query, index, idf, doc_norms, score_func=acc_dot_scores):
+    ans = []
+    q = tokenize(query)
+    tmp = {}
+    qnorm = 0
+    for word in q:
+        tmp[word] = q.count(word)
+        if word in idf:
+            qnorm += (tmp[word] * idf[word])**2
+        qnorm = math.sqrt(qnorm)
+    dots = score_func(tmp, index, idf)
+    for doc in range(len(doc_norms)):
+        score = -1
+        if doc in dots:
+            score = round(float(dots[doc])/ (qnorm * doc_norms[doc]), 2)
+        ans += [(score, doc)]
+    ans.sort(key = lambda x: x[0], reverse = True)
+    return ans
+        
+
+# name_tokens= {name:tokenize(name) for name in names}
+def cosine_sim(query):
+    ans = []
+    products= request.args.get("product_name") #change if want review
+    inv_idx = build_inv_ind(products)
+    idf = compute_idf(inv_idx, len(products), 10, 0.1)
+    inv_idx = {key: val for key, val in inv_idx.items()
+           if key in idf} 
+    norms = compute_norms(inv_idx, idf, len(products))
+    for _, id in index_search(query, inv_idx, idf, norms)[:10]:
+        ans+=[products[id]]
+    return ans
 
 app.run(debug=True)
